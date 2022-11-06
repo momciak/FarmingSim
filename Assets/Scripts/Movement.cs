d1259349a1e79c241dc030ed07137959b8c66f6a
momciak <dr.czajnik@gmail.com>
2022-11-07 00:41:20 +0100

    Added assets

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    public Animator animator;
+
+    private Vector3 direction;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector3(0, 0);
+        if (horizontal == 0) direction = new Vector3(0, vertical);
+        if (vertical == 0) direction = new Vector3(horizontal, 0);
+
+        AnimateMovement(direction);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+    void AnimateMovement(Vector3 direction)
+    {
+        if(animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
+    }
+}
+
