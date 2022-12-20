532520e371bd7fe4c05aee8936d9fdf0f797350c
momciak <dr.czajnik@gmail.com>
2022-12-21 00:53:32 +0100

    Added Inventory and extra tags

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CollectableType
+{
+    None,
+    SunflowerSeed
+}
 public class Collectable : MonoBehaviour
 {
+    public CollectableType type;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
 {
     public int numSunflowerSeed = 0;
 
+    public Inventory inventory;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Sunflower")) return;
 
         numSunflowerSeed++;
+    }
+    private void Awake()
+    {
+        inventory = new Inventory(21);
     }
 }
